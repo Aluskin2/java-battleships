d795284b708cae3d586cfb23531525644edb88a7
Alan Przybysz <alan.przybysz@wakacje.pl>
2022-11-18 20:58:31 +0100

    dodanie szkieletu skryptu i rozpisanie tasków

---
+++ src/BattleShips.java
@@ -0,0 +1,55 @@
+public class BattleShips
+{
+    public static int battleGroundWidth = 8;
+    public static int battleGroundLength = 8;
+    public static int playerShips;
+    public static int computerShips;
+    public static String[][] playerBattleGround = new String[battleGroundWidth][battleGroundLength];
+    public static String[][] computerBattleGround = new String[battleGroundWidth][battleGroundLength];
+    public static String[][] playerMissedShotsGround = new String[battleGroundWidth][battleGroundLength];
+
+    public static void main(String[] args)
+    {
+        //TODO spięcie pozostałych metod wewnętrz
+    }
+
+    public static void createBattlegrounds()
+    {
+        //TODO https://github.com/Aluskin2/java-battleships/issues/1
+    }
+
+    public static void fillPlayerBattleground()
+    {
+        //TODO https://github.com/Aluskin2/java-battleships/issues/3
+    }
+
+    public static void fillComputerBattleground()
+    {
+        //TODO https://github.com/Aluskin2/java-battleships/issues/6
+    }
+
+    public static void playerAttack()
+    {
+        //TODO https://github.com/Aluskin2/java-battleships/issues/4
+    }
+
+    public static void computerAttack()
+    {
+        //TODO https://github.com/Aluskin2/java-battleships/issues/5
+    }
+
+    public static void getPlayerBattlegroundView()
+    {
+        //TODO https://github.com/Aluskin2/java-battleships/issues/2
+    }
+
+    public static void getPlayerShotsView()
+    {
+        //TODO https://github.com/Aluskin2/java-battleships/issues/7
+    }
+
+    public static void endGame()
+    {
+        //TODO https://github.com/Aluskin2/java-battleships/issues/8
+    }
+}
