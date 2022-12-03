86b314f8d299b5155e867cf0aaf6cd25721cf7fa
Alan Przybysz <alan.przybysz@wakacje.pl>
2022-12-03 13:00:06 +0100

    dodanie tworzenia i wyswietlenia plansz do gry

--- src/BattleShips.java
+++ src/BattleShips.java
@@ -1,21 +1,73 @@
 public class BattleShips
 {
-    public static int battleGroundWidth = 8;
-    public static int battleGroundLength = 8;
+    public static int battleGroundColumns = 8;
+    public static int battleGroundRows = 8;
+
     public static int playerShips;
     public static int computerShips;
-    public static String[][] playerBattleGround = new String[battleGroundWidth][battleGroundLength];
-    public static String[][] computerBattleGround = new String[battleGroundWidth][battleGroundLength];
-    public static String[][] playerMissedShotsGround = new String[battleGroundWidth][battleGroundLength];
+
+    public static String[][] playerBattleGround = new String[battleGroundColumns][battleGroundRows];
+    public static String[][] playerShotsGround = new String[battleGroundColumns][battleGroundRows];
+    public static String[][] computerBattleGround = new String[battleGroundColumns][battleGroundRows];
 
     public static void main(String[] args)
     {
+        createBattlegrounds();
+
         //TODO spięcie pozostałych metod wewnętrz
     }
 
     public static void createBattlegrounds()
     {
-        //TODO https://github.com/Aluskin2/java-battleships/issues/1
+        System.out.print("PLAYER BATTLEGROUND");
+        System.out.println();
+        System.out.print("  ");
+
+        createBattleground(playerBattleGround, true);
+        System.out.println();
+
+        System.out.print("PLAYER SHOTS / ENEMY BATTLEGROUND");
+        System.out.println();
+        System.out.print("  ");
+
+        createBattleground(playerShotsGround, true);
+        System.out.println();
+
+        createBattleground(computerBattleGround, false);
+    }
+
+    private static void createBattleground(String[][] battleground, Boolean print)
+    {
+        if (print) {
+            for (int i = 1; i <= battleGroundColumns; i++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < battleground.length; i++) {
+            for (int j = 0; j < battleground[i].length; j++) {
+                battleground[i][j] = "O";
+                if (print) {
+                    if (j == 0) {
+                        System.out.print((i + 1) + "|" + battleground[i][j]);
+                    } else if (j == battleground[i].length - 1) {
+                        System.out.print(battleground[i][j] + "|" + (i + 1));
+                    } else {
+                        System.out.print(battleground[i][j]);
+                    }
+                }
+            }
+            System.out.println();
+        }
+
+        if (print) {
+            System.out.print("  ");
+            for (int i = 1; i <= battleGroundColumns; i++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
     }
 
     public static void fillPlayerBattleground()
